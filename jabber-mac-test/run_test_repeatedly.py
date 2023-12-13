ba2c82f668dead7fbdd1011d4b71dc554c6302bf
czlitony <czli.tony@gmail.com>
2023-12-13 19:44:24 +0800

    Update run_test_repeatedly.py

--- jabber-mac-test/run_test_repeatedly.py
+++ jabber-mac-test/run_test_repeatedly.py
@@ -3,22 +3,25 @@ This file is used to do hello world service test
 """
 
 import subprocess
+import os
 import sys
 import time
 
 def run_test(program):
     """do test"""
-    t0 = time.time()
+    start_time = time.time()
 
     command = program + " > repeat_test.log 2>&1"
 
     sp = subprocess.Popen(command, shell=True)
     sp.communicate()
     if sp.returncode != 0:
-        return False
+        return False, 0
 
-    print("Finished: " + str(time.time() - t0) + " seconds\n")
-    return True
+    end_time = time.time()
+    time_cost = end_time - start_time
+    print("Time cost: " + str(time_cost) + " seconds")
+    return True, time_cost
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -30,14 +33,14 @@ if __name__ == '__main__':
         total_time_cost = 0
         for index in range(1, 1 + 100):
             print('Running ' + program + ': ' + str(index))
-            start_time = time.time()
-            succeeded = run_test(sys.argv[1])
-            end_time = time.time()
-            time_cost = end_time - start_time
-            print('succeeded: ' + str(succeeded) + ', time cost: ' + str(time_cost))
+            succeeded, time_cost = run_test(sys.argv[1])
+            print('Succeeded: ' + str(succeeded))
+            print('')
             if succeeded:
                 success_cnt += 1
                 total_time_cost += time_cost
             else:
                 failure_cnt += 1
-        print('success_cnt: ' + str(success_cnt) + ', total time cost: ' + str(total_time_cost) + ', failure_cnt: ' + str(failure_cnt))
+                if os.path.exists('current_log.txt'):
+                    os.rename('current_log.txt', 'failure_' + str(time.time()) + '_current_log.txt')
+        print('Success count: ' + str(success_cnt) + ', total time cost: ' + str(total_time_cost) + ', failure count: ' + str(failure_cnt))
